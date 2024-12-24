4340c6f4ab02f2203a55bb5a71f5aa055c3c548f
oneofthezombies <hunhoekim@gmail.com>
2024-12-24 12:31:42 +0000

    wip: node

--- packages/test-node/vitest.config.ts
+++ packages/test-node/vitest.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vitest/config";
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait()],
   test: {
     environment: "edge-runtime",
   },
