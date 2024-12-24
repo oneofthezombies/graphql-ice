f7a5d5a491688594aab714ec5f3255cbc7ad9415
oneofthezombies <hunhoekim@gmail.com>
2024-12-24 12:37:36 +0000

    wip: test

--- packages/test-node/src/ping.test.ts
+++ packages/test-node/src/ping.test.ts
@@ -1,8 +1,9 @@
 import { test, expect } from "vitest";
 import { createEngine } from "@graphql-ice/engine/adapter/node";
 
+const engine = await createEngine();
+
 test("ping", async () => {
-  const engine = await createEngine();
   const result = await engine.ping();
   expect(result).toBe("pong");
 });
