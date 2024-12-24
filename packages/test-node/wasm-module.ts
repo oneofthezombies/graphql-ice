5bc418ac9d48704b916fe5c696bb2e899774d876
oneofthezombies <hunhoekim@gmail.com>
2024-12-24 12:30:46 +0000

    wip: node

--- packages/test-node/wasm-module.ts
+++
@@ -1,6 +0,0 @@
-import { Engine } from "@graphql-ice/engine";
-import core from "@graphql-ice/engine/core.wasm" assert { type: "webassembly" };
-
-const engine = new Engine(core);
-const result = await engine.ping();
-console.log(result);
