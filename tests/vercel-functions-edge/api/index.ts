a8b7852ced03d20f82baa4cc5c11a0038f6bc80d
oneofthezombies <hunhoekim@gmail.com>
2024-12-27 14:24:26 +0000

    wip

--- tests/vercel-functions-edge/api/index.ts
+++ tests/vercel-functions-edge/api/index.ts
@@ -1,5 +1,11 @@
+/// <reference types="@edge-runtime/types" />
+
 import { Engine } from "@graphql-ice/engine";
 import core from "@graphql-ice/engine/core.wasm?module";
+
+if (typeof EdgeRuntime !== "string") {
+  throw new Error("Runtime is not edge");
+}
 
 export const config = {
   runtime: "edge",
