77d98e3a9b60e41c4184c6ae14054941dfbea632
oneofthezombies <hunhoekim@gmail.com>
2024-12-27 14:18:10 +0000

    wip

--- tests/vercel-functions-edge/api/index.ts
+++ tests/vercel-functions-edge/api/index.ts
@@ -1,14 +1,13 @@
 import { Engine } from "@graphql-ice/engine";
-
-// @ts-expect-error
 import core from "@graphql-ice/engine/core.wasm?module";
 
 export const config = {
   runtime: "edge",
 };
 
-const engine = new Engine(core);
+const { graphql } = await Engine.init({ core });
 
 export async function GET(request: Request) {
-  return new Response(await engine.ping());
+  const result = await graphql({ schema: {}, source: "" });
+  return new Response(JSON.stringify(result));
 }
