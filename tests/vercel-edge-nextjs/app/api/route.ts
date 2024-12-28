8bbe4c7e0a32bb0ae59285ebd2dc404b83707495
oneofthezombies <hunhoekim@gmail.com>
2024-12-28 11:42:33 +0000

    wip

---
+++ tests/vercel-edge-nextjs/app/api/route.ts
@@ -0,0 +1,12 @@
+import { Engine } from "@graphql-steel/engine";
+import core from "@graphql-steel/engine/core.wasm?module";
+
+export const dynamic = "force-dynamic";
+export const runtime = "edge";
+
+const { graphql } = await Engine.init({ core });
+
+export async function GET(request: Request) {
+  const result = await graphql({ schema: {}, source: "" });
+  return new Response(JSON.stringify(result));
+}
