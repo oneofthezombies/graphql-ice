740f63df3182141256c47ccfca0bb4981d8f079e
oneofthezombies <hunhoekim@gmail.com>
2024-12-28 11:35:57 +0000

    wip

--- workspaces/vercel-edge-nextjs/app/api/route.ts
+++ workspaces/vercel-edge-nextjs/app/api/route.ts
@@ -1,18 +1,11 @@
 import { Engine } from "@graphql-steel/engine";
-// @ts-expect-error
 import core from "@graphql-steel/engine/core.wasm?module";
 
-// // @ts-expect-error
-// import core from "@/public/core_bg.wasm?module";
-
 export const dynamic = "force-dynamic";
-export const runtime = "edge";
+
+const { graphql } = await Engine.init({ core });
 
 export async function GET(request: Request) {
-  const result = core;
-  // const core = await import("@graphql-steel/engine/core.wasm?module");?
-  // console.log("@", core);
-  // const { graphql } = await Engine.init({ core });
-  // const result = await graphql({ schema: {}, source: "" });
+  const result = await graphql({ schema: {}, source: "" });
   return new Response(JSON.stringify(result));
 }
