2eed146c271cfb8c497975870b32b67c20e5c606
oneofthezombies <hunhoekim@gmail.com>
2024-12-29 04:44:44 +0000

    wip

--- packages/fix-cloudflare-vitest-pool-workers-plugin/vite.config.ts
+++ packages/fix-cloudflare-vitest-pool-workers-plugin/vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import deletePlugin from "rollup-plugin-delete";
 import typescript from "vite-plugin-typescript";
 
-const isTest = process.env.MODE === "test";
+const isTest = process.env.NODE_ENV === "test";
 
 export default defineConfig({
   plugins: [
-    isTest && deletePlugin({ targets: "dist/*", runOnce: true }),
+    !isTest && deletePlugin({ targets: "dist/*", runOnce: true }),
     typescript({
-      tsconfig: "tsconfig.json",
+      tsconfig: !isTest ? "tsconfig.build.json" : "tsconfig.json",
     }),
   ].filter(Boolean),
   build: {
