cce696dfbd39e8f9f8ac45bbbcd8a8ac0973768f
oneofthezombies <hunhoekim@gmail.com>
2024-12-29 11:09:00 +0000

    wip

--- examples/deno-deploy/main.ts
+++ examples/deno-deploy/main.ts
@@ -1,4 +1,8 @@
-import { initOnce } from "@graphql-steel/steel";
+import { graphql, initOnce } from "@graphql-steel/steel";
 import core from "@graphql-steel/steel/core_bg.wasm";
 
-Deno.serve(() => new Response("Hello, world!"));
+Deno.serve(async () => {
+  await initOnce(core);
+  const result = await graphql({ schema: {}, source: "" });
+  return new Response(JSON.stringify(result));
+});
