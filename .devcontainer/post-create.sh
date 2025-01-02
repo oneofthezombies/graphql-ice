8c81dd8c8adb46ec9f9975b5c182b168809a4398
oneofthezombies <hunhoekim@gmail.com>
2025-01-02 15:18:01 +0900

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,7 +3,7 @@ set -ex
 
 npm install --workspaces
 
-POST_CREATE_SCRIPT="post-create.sh"
+POST_CREATE_SCRIPT="/graphql-steel/post-create.sh"
 if [[ -f "$POST_CREATE_SCRIPT" ]]; then
     chmod +x "$POST_CREATE_SCRIPT"
     "$POST_CREATE_SCRIPT"
