fe6adcbefe9a718fc9dc7b212fdb38cdd69fd7f3
oneofthezombies <hunhoekim@gmail.com>
2025-01-02 06:14:02 +0000

    wip

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,3 +2,9 @@
 set -ex
 
 npm install --workspaces
+
+POST_CREATE_SCRIPT="post-create.sh"
+if [[ -f "$POST_CREATE_SCRIPT" ]]; then
+    chmod +x "$POST_CREATE_SCRIPT"
+    "$POST_CREATE_SCRIPT"
+fi
